cd7c334bd3b9062d1aa1ede55f61a9d0139e01cc
Kosenko <alexandkosenko@gmail.com>
2024-09-08 15:48:10 +0300

    basic_structs.c/.h for basic terminal frawing structures

---
+++ src/terminal_UI/basic_structs.c
@@ -0,0 +1,5 @@
+//
+// Created by kosenko on 9/8/24.
+//
+
+#include "basic_structs.h"
---
+++ src/terminal_UI/basic_structs.h
@@ -0,0 +1,13 @@
+//
+// Created by kosenko on 9/8/24.
+//
+
+#ifndef BFO_CLI_READING_LIST_SRC_TERMINAL_UI_BASIC_STRUCTS_H
+#define BFO_CLI_READING_LIST_SRC_TERMINAL_UI_BASIC_STRUCTS_H
+
+typedef struct
+{
+    unsigned int row_start, row_end, col_start, col_end;
+} row_col_rectangle;
+
+#endif //BFO_CLI_READING_LIST_SRC_TERMINAL_UI_BASIC_STRUCTS_H
